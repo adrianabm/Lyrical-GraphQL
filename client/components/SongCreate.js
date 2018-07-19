80997746342a87d6e67fd83f52bd604aa5bd7d48
Adriana Matsufuji <adrianabm@gmail.com>
2018-07-19 15:04:20 +0200

    Add mutation to create songs inside SongCreate component

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class SongCreate extends Component {
   constructor(props) {
@@ -9,11 +11,21 @@ class SongCreate extends Component {
     }
   }
 
+  onSubmit(event) {
+    event.preventDefault()
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song title:</label>
           <input
             onChange={event => this.setState({ title: event.target.value })}
@@ -25,4 +37,12 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`
+
+export default graphql(mutation)(SongCreate)
