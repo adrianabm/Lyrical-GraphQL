e8f1a0a63b8b962ae8b884d798f21cbfe3516c38
Adriana Matsufuji <adrianabm@gmail.com>
2018-07-20 11:00:51 +0200

    Add some navigation between screens

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 
 import fetchSongDetail from '../queries/fetchSongDetail'
 
 class SongDetail extends Component {
   render() {
+    const { song } = this.props.data
+
+    if (!song) {
+      // also possible to use this.props.data.loading
+      return <div>Loading...</div>
+    }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
       </div>
     )
   }
