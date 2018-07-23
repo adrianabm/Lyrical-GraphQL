70537a6b04d5d2854a345fb78fb8e8259b607cdf
Adriana Matsufuji <adrianabm@gmail.com>
2018-07-23 10:03:14 +0200

    Turn usePushEach on for all mongoose models

--- server/models/index.js
+++ server/models/index.js
@@ -1,2 +1,7 @@
-require('./song');
-require('./lyric');
+const mongoose = require('mongoose')
+mongoose.plugin(schema => {
+  schema.options.usePushEach = true
+})
+
+require('./song')
+require('./lyric')
